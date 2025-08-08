8c3c9ebfa14356aa74813d0dd4498d40e7ff573c
AnaLauraCavalheiroMiranda <ana.cavalheiro.miranda@escola.pr.gov.br>
2025-08-07 21:27:15 -0300

    Add files via upload

---
+++ cafe-main/script.js
@@ -0,0 +1,11 @@
+Selecione()
+{
+let ingredientes = [
+  { id:"chocolate",item: "chocolate_branco", quantidade: "5", preço: 85 }
+];
+   document.getElementById('chocolate').textContent = "chocolate: " + chocolate;
+}
+
+  function removerPlaceholder(selectElement) {
+    const primeiraOpcao = selectElement.options[0];
+    }
