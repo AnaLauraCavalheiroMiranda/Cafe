4ad71251e59f03b56929ef0331617f35b0f9c263
AnaLauraCavalheiroMiranda <ana.cavalheiro.miranda@escola.pr.gov.br>
2025-08-21 17:19:50 -0300

    Add files via upload

---
+++ Cafe-main/Cafe-main/Cafe-main/script.js
@@ -0,0 +1,38 @@
+Terminar_pedido()
+{
+  //vetores de objeto
+
+
+const opcoes_de_cafe = [
+
+  { id:"cafe_comum", nome: "Cafe comum", preco: 1 },
+  { id:"descafeinado", nome: "Cafe descafeinado", preco: 2 },
+  { id:"cafeina_adicionada", nome: "Cafe com cafeína adicionada", preco: 3 },
+  { id:"Café_leite", nome: "Cafe com leite", preco: 4 },
+  { id:"Cafe_expresso", nome: "Cafe expresso", preco: 5 },
+  { id:"Cafe_instantaneo", nome: "Cafe instantaneo", preco: 6 },
+  { id:"Cafe_gourmet", nome:"Cafe gourmet", preco: 7 },
+  { id:"Capuccino", nome:"Capuccino", preco: 8 },
+  { id:"Mocha", nome:"Mocha", preco: 9 }
+  
+];
+
+const chocolates = [
+  { id:"chocolate", nome: "chocolate ao leite", preco: 3 }
+
+];
+}
+  {
+   document.getElementById('chocolate').textContent = "chocolate: " + chocolate;
+}
+
+//Parte para não usar a mesma opção "selecione" deixando em branco.
+  function removerPlaceholder(selectElement) {
+    const primeiraOpcao = selectElement.options[0];
+    }
+
+//tentando mostrar aprecer
+    {
+    alert (cafe_comum.preco)
+
+  } 
